eb66e2123a6069bf87ec4fe9f41e0c1250d4289f
magalieyang <magalie.dib.yang@gmail.com>
2025-04-24 23:18:59 +0200

    chore: install depedencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,30 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
+import tailwindcss from '@tailwindcss/vite';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	build: {
+		transpile: ['vuetify'],
+	},
+	modules: [
+		'@nuxt/eslint',
+		(_options, nuxt) => {
+			nuxt.hooks.hook('vite:extendConfig', (config) => {
+				// @ts-expect-error expect error
+				config.plugins.push(vuetify({ autoImport: true }));
+			});
+		},
+	],
+	css: ['@/assets/css/main.css'],
+	vite: {
+		plugins: [tailwindcss()],
+		vue: {
+			template: {
+				transformAssetUrls,
+			},
+		},
+	},
+});
