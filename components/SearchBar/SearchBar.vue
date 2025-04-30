c8acdba0c7f687c2161ee4910372dcedc5c1c055
magalieyang <magalie.dib.yang@gmail.com>
2025-04-30 22:30:48 +0200

    mend

--- components/SearchBar/SearchBar.vue
+++ components/SearchBar/SearchBar.vue
@@ -32,7 +32,7 @@ import { useRouter } from 'vue-router';
 import { ref } from 'vue';
 
 const loading = ref<boolean>(false);
-const items = ref<string[]>([]);
+const items = ref<{ title: string; date: string; originalTitle: string; value: number }[]>([]);
 const router = useRouter();
 const selectedMovie = ref(null);
 
